712b15a6fde2e79e89a44a952408800b0613c99e
Peter Ekimseev <peter.ekimseev@mail.ru>
2023-03-10 22:26:02 +0300

    Пятое задание полностью готово, новая корректировка

--- 5.classes/task.js
+++ 5.classes/task.js
@@ -1,10 +1,10 @@
 class PrintEditionItem{
   constructor(name, releaseDate, pagesCount){
-	this.name = name;
-	this.releaseDate = releaseDate;
-	this.pagesCount = pagesCount;
-	this.state = 100;
-	this.type = null;
+    this.name = name;
+    this.releaseDate = releaseDate;
+    this.pagesCount = pagesCount;
+    this.state = 100;
+    this.type = null;
   }
   fix(){
     this.state *= 1.5;
