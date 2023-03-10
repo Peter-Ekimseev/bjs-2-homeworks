21bf0515cb0b5903deea9c039dfb837f4a80971c
Peter Ekimseev <peter.ekimseev@mail.ru>
2023-03-10 22:29:47 +0300

    Пятое задание полностью готово, ещё корректировка

--- 5.classes/task.js
+++ 5.classes/task.js
@@ -25,43 +25,43 @@ class PrintEditionItem{
 
 class Magazine extends PrintEditionItem{
   constructor(name, releaseDate, pagesCount){
-	super(name, releaseDate, pagesCount);
-	this.type = "magazine";
+    super(name, releaseDate, pagesCount);
+    this.type = "magazine";
   }
 }
 
 class Book extends PrintEditionItem{
   constructor(author, name, releaseDate, pagesCount){
-	super(name, releaseDate, pagesCount);
+    super(name, releaseDate, pagesCount);
     this.author = author;
-	this.type = "book";
+    this.type = "book";
   }
 }
 
 class NovelBook extends Book{
   constructor(author, name, releaseDate, pagesCount){
-	super(author, name, releaseDate, pagesCount);
-	this.type = "novel";
+    super(author, name, releaseDate, pagesCount);
+    this.type = "novel";
   }
 }
 
 class FantasticBook extends Book{
   constructor(author, name, releaseDate, pagesCount){
-	super(author, name, releaseDate, pagesCount);
-	this.type = "fantastic";
+    super(author, name, releaseDate, pagesCount);
+    this.type = "fantastic";
   }
 }
 
 class DetectiveBook extends Book{
   constructor(author, name, releaseDate, pagesCount){
-	super(author, name, releaseDate, pagesCount);
-	this.type = "detective";
+    super(author, name, releaseDate, pagesCount);
+    this.type = "detective";
   }
 }
 
 class Library{
   constructor(name, books){
-	this.name = name;
+    this.name = name;
     this.books = [];
   }
   addBook(book){
